2257350c8508fc48ce87f179b6dfaea042dc05b1
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-07-18 15:07:32 +0200

    .

--- src/main/java/Entities/PuntoVendita.java
+++ src/main/java/Entities/PuntoVendita.java
@@ -1,6 +1,5 @@
 package Entities;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.DiscriminatorColumn;
@@ -22,31 +21,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "tipo punto vendita")
-@Table(name = "Punti vendita")
 @DiscriminatorColumn(name = "tipo_punto_vendita")
 @Table(name = "Punti_vendita")
 @NoArgsConstructor
 public class PuntoVendita {
 
 	@Id
-	@SequenceGenerator(name = "my_sequence", sequenceName = "my_sequence", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_sequence")
 	@SequenceGenerator(name = "my_sequence2", sequenceName = "my_sequence2", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_sequence2")
 	private long id;
 
-	private Biglietti biglietti;
-	private Abbonamenti abbonamenti;
-	static int numeroBiglietti;
-	static int numeroAbbonamenti;
-	private LocalDate dataEmissione;
-
-	public PuntoVendita(Biglietti biglietti, Abbonamenti abbonamenti) {
-		super();
-		this.biglietti = biglietti;
-		this.abbonamenti = abbonamenti;
-	}
 	@OneToMany(mappedBy = "puntoVendita")
 	private List<Biglietti> biglietti;
 	@OneToMany(mappedBy = "puntoVendita")
