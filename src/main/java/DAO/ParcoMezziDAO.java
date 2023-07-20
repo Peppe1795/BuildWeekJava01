636950122a90287f0adcff03b07de8752498cd83
Peppe1795 <petrucci238@gmail.com>
2023-07-20 11:35:35 +0200

    metodo update

--- src/main/java/DAO/ParcoMezziDAO.java
+++ src/main/java/DAO/ParcoMezziDAO.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import Entities.ParcoMezzi;
-import Entities.Utente;
+import Entities.StatoMezzi;
 
 public class ParcoMezziDAO {
 	private final EntityManager em;
@@ -26,13 +26,38 @@ public class ParcoMezziDAO {
 		System.out.println("Elemento salvato correttamente");
 
 	}
-	
+
 	public List<ParcoMezzi> visualizzaParcoMezzi() {
-	    return em.createQuery("SELECT r FROM ParcoMezzi r", ParcoMezzi.class)    
-	            .getResultList();
+		return em.createQuery("SELECT r FROM ParcoMezzi r", ParcoMezzi.class).getResultList();
 	}
+
 	public ParcoMezzi ricercaMezziDaId(Long id) {
-        return em.find(ParcoMezzi.class, id);
-    }
-	
+		return em.find(ParcoMezzi.class, id);
+	}
+
+	public void findMezzoById(Long id) {
+		ParcoMezzi found = em.find(ParcoMezzi.class, id);
+
+		if (found != null) {
+			System.out.println(found);
+		} else {
+			System.out.println("Elemento con id " + id + " non trovato");
+		}
+
+	}
+
+	public void updateStatoMezzo(Long id) {
+		ParcoMezzi found = em.find(ParcoMezzi.class, id);
+		EntityTransaction t = em.getTransaction();
+		if (found != null) {
+			t.begin();
+			if (found.getStatoMezzi() == StatoMezzi.IN_MANUTENZIONE) {
+				found.setStatoMezzi(StatoMezzi.IN_SERVIZIO);
+			} else if (found.getStatoMezzi() == StatoMezzi.IN_SERVIZIO) {
+				found.setStatoMezzi(StatoMezzi.IN_MANUTENZIONE);
+			}
+			t.commit();
+		}
+
+	}
 }
--- src/main/java/Entities/ParcoMezzi.java
+++ src/main/java/Entities/ParcoMezzi.java
@@ -25,40 +25,41 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ParcoMezzi {
 
-    @Id
-    @SequenceGenerator(name = "my_sequence3", sequenceName = "my_sequence3", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_sequence3")
-    @Column(name = "Id")
-    private Long id;
+	@Id
+	@SequenceGenerator(name = "my_sequence3", sequenceName = "my_sequence3", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_sequence3")
+	@Column(name = "Id")
+	private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private TipoMezzo tipoMezzo;
+	@Enumerated(EnumType.STRING)
+	private TipoMezzo tipoMezzo;
 
-    @Enumerated(EnumType.STRING)
-    private StatoMezzi statoMezzi;
+	@Enumerated(EnumType.STRING)
+	private StatoMezzi statoMezzi;
 
-    private LocalDate inizioStatoMezzo;
-    private int capienza;
+	private LocalDate inizioStatoMezzo;
+	private int capienza;
 
-    @OneToOne(mappedBy = "parcoMezzi")
-    private Manutenzione manutenzione;
+	@OneToOne(mappedBy = "parcoMezzi")
+	private Manutenzione manutenzione;
 
-    @OneToOne(mappedBy = "parcoMezzi")
-    private Tratta tratta;
+	@OneToOne(mappedBy = "parcoMezzi")
+	private Tratta tratta;
 
-    @OneToMany(mappedBy = "puntoVidimazione")
-    private List<Biglietti> biglietti = new ArrayList<>();;
+	@OneToMany(mappedBy = "puntoVidimazione")
+	private List<Biglietti> biglietti = new ArrayList<>();
 
-    public ParcoMezzi(TipoMezzo tipoMezzo, StatoMezzi statoMezzi, LocalDate inizioStatoMezzo, int capienza, Tratta tratta) {
-        this.tipoMezzo = tipoMezzo;
-        this.statoMezzi = statoMezzi;
-        this.capienza = capienza;
-        this.tratta = tratta;
-    }
-    
-    @Override
+	public ParcoMezzi(TipoMezzo tipoMezzo, StatoMezzi statoMezzi, LocalDate inizioStatoMezzo, int capienza,
+			Tratta tratta) {
+		this.tipoMezzo = tipoMezzo;
+		this.statoMezzi = statoMezzi;
+		this.capienza = capienza;
+		this.tratta = tratta;
+	}
+
+	@Override
 	public String toString() {
-		return "tipo mezzo:" + tipoMezzo + ", id:"+ id + ", stato mezzi:" + statoMezzi + ", tratta:" + tratta;
+		return "tipo mezzo:" + tipoMezzo + ", id:" + id + ", stato mezzi:" + statoMezzi + ", tratta:" + tratta;
 	}
 
 }
